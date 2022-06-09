afc5563bbc11caa29e952ef37263ee37f23d548a
Mysteryexe <99576711+Mysteryexe@users.noreply.github.com>
2022-06-09 18:09:12 +0430

    Update addneeded.js

--- content/needed/addneeded.js
+++ content/needed/addneeded.js
@@ -1,4 +1,4 @@
 //navbar
-$(".add-nav").load(".../content/needed/nav.html");
+$(".add-nav").load("./content/needed/nav.html");
 //footer undone
 //$(".add-footer").load("/content/needed/footer.html");
