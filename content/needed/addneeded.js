d4ce55b7fa2da94c7ea9cf38ee7c4d90597a91b3
Mysteryexe <99576711+Mysteryexe@users.noreply.github.com>
2023-01-06 07:27:37 +0330

    Add files via upload

--- content/needed/addneeded.js
+++ content/needed/addneeded.js
@@ -1,4 +1,4 @@
 //navbar
 $(".add-nav").load("./content/needed/nav.html");
-//footer undone
-//$(".add-footer").load("/content/needed/footer.html");
+//footer
+$(".add-footer").load("/content/needed/footer.html");
